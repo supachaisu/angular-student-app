a3f70e8977af5df75eab74a022f19a433cdd9a24
supachaisu <180935381+supachaisu@users.noreply.github.com>
2024-11-12 11:33:24 +0700

    implement add, edit, delete functionalities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,21 +1,65 @@
 import { Component, inject } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
 import { StudentService } from './student.service';
 import { Student } from './student';
+import {
+  FormControl,
+  FormGroup,
+  ReactiveFormsModule,
+  Validators,
+  FormsModule,
+} from '@angular/forms';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [ReactiveFormsModule, FormsModule],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
   title = 'student-app';
-  students: Student[] = [];
   studentService = inject(StudentService);
 
-  constructor() {
-    this.students = this.studentService.getStudents();
+  get students(): Student[] {
+    return this.studentService.getStudents();
+  }
+
+  addStudentForm = new FormGroup({
+    firstName: new FormControl('', Validators.required),
+    lastName: new FormControl('', Validators.required),
+  });
+
+  submitAddStudentForm(): void {
+    if (this.addStudentForm.invalid) {
+      // Find first invalid input and focus it
+      const firstInvalidInput = document.querySelector(
+        'form input:invalid'
+      ) as HTMLInputElement;
+      firstInvalidInput?.focus();
+      return;
+    }
+
+    if (
+      !this.addStudentForm.value.firstName ||
+      !this.addStudentForm.value.lastName
+    ) {
+      return;
+    }
+
+    this.studentService.addStudent({
+      id: crypto.randomUUID(),
+      firstName: this.addStudentForm.value.firstName,
+      lastName: this.addStudentForm.value.lastName,
+      isEditing: false,
+    });
+  }
+
+  toggleEdit(student: Student): void {
+    student.isEditing = !student.isEditing;
+    this.studentService.editStudent(student);
+  }
+
+  deleteStudent(student: Student): void {
+    this.studentService.removeStudent(student);
   }
 }
--- src/app/student.ts
+++ src/app/student.ts
@@ -1,5 +1,6 @@
 export interface Student {
   id: string;
-  name: string;
+  firstName: string;
   lastName: string;
+  isEditing: boolean;
 }
